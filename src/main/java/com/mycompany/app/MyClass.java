6c2eab74b7d30961f4c0928ee9e6f2e33b402abb
Federico Benitez <federico.benitez@gteamstaff.com>
2016-05-09 10:54:16 -0300

    new class

---
+++ src/main/java/com/mycompany/app/MyClass.java
@@ -0,0 +1,81 @@
+package com.mycompany.app;
+
+import java.io.Console;
+
+public class MyClass {
+	public int SomeMethod(){
+		int x = 0;
+		int y = 0;
+		int result;
+		
+		x = 3;
+		y = 4;
+		
+		result = x + y;
+		
+		result = 2* result;
+		
+		return result;
+	}
+	
+	public int SomeMethod2(){
+		int x = 0;
+		int y = 0;
+		int result;
+		
+		x = 3;
+		y = 4;
+		
+		result = x + y;
+		
+		result = 2* result;
+		
+		return result;
+	}
+	
+	public int SomeMethod3(){
+		int x = 0;
+		int y = 0;
+		int result;
+		
+		x = 3;
+		y = 4;
+		
+		result = x + y;
+		
+		result = 2* result;
+		
+		return result;
+	}
+	
+	public int SomeMethod4(){
+		int x = 0;
+		int y = 0;
+		int result;
+		
+		x = 3;
+		y = 4;
+		
+		result = x + y;
+		
+		result = 2* result;
+		
+		return result;
+	}
+	
+	public int SomeMethod5(){
+		int x = 0;
+		int y = 0;
+		int result;
+		
+		x = 3;
+		y = 4;
+		
+		result = x + y;
+		
+		result = 2* result;
+		
+		return result;
+	}
+	
+}
